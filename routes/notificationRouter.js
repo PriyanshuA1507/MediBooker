d996817e53e0ca58a47df2d597a59134491a1351
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-11-10 17:36:57 +0530

    Update notificationRouter.js

--- routes/notificationRouter.js
+++ routes/notificationRouter.js
@@ -5,9 +5,9 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
 
-const { getallnotifs } = require("../controllers/notificationController");
+const { getAllNotifications } = require("../controllers/notificationController");
 
 // Fetch all notifications for logged-in user
-router.get("/getallnotifs", auth, getallnotifs);
+router.get("/getallnotifs", auth, getAllNotifications);
 
 module.exports = router;
