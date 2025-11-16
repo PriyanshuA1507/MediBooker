30ab810d8decf7414595fca6f382fa0aeb471ea8
Priyanshu <97669354+PriyanshuA1507@users.noreply.github.com>
2025-11-17 02:13:21 +0530

    Update doctorRoutes.js

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -11,11 +11,16 @@ const {
   deleteDoctor,
 } = require("../controllers/doctorController");
 
+// PUBLIC ROUTES
 router.get("/getalldoctors", getAllDoctors);
+
+// ADMIN ROUTES
 router.get("/getnotdoctors", auth, getNotDoctors);
-router.post("/applyfordoctor", auth, applyForDoctor);
-router.delete("/deletedoctor", auth, deleteDoctor);
 router.put("/acceptdoctor", auth, acceptDoctor);
 router.put("/rejectdoctor", auth, rejectDoctor);
+router.delete("/deletedoctor/:id", auth, deleteDoctor);  // 🔥 FIXED ROUTE
+
+// DOCTOR APPLY
+router.post("/applyfordoctor", auth, applyForDoctor);
 
 module.exports = router;
